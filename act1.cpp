17e7bb2cce0f4796b9f73504965db442171b0eb4
joms021062 <156154075+joms021062@users.noreply.github.com>
2024-01-18 19:01:08 +0800

    Create act1.cpp

---
+++ act1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib> 
+
+using namespace std;
+
+int main() {
+    int num1, num2, result;
+    char choice;
+    char cont;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    do {
+        cout << "\nMenu\n";
+        cout << "1. Add\n";
+        cout << "2. Subtract\n";
+        cout << "3. Multiply\n";
+        cout << "4. Divide\n";
+        cout << "5. Modulus\n";
+        cout << "6. Power\n";
+        cout << "7. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case '1':
+            result = num1 + num2;
+            break;
+        case '2':
+            result = num1 - num2;
+            break;
+        case '3':
+            result = num1 * num2;
+            break;
+        case '4':
+            if (num2 == 0) {
+                cout << "Error: Division by zero is not allowed." << endl;
+                continue;
+            }
+            result = num1 / num2;
+            break;
+        case '5':
+            result = num1 % num2;
+            break;
+        case '6':
+            result = pow(num1, num2);
+            break;
+        case '7':
+            cout << "Exiting..";
+            break;
+        default:
+            cout << "Invalid choice! Please choose a valid operation.";
+            continue;
+        }
+        cout << "The result is: " << result << endl;
+
+        if (choice != '7') {
+
+            cout << "\nDo you wish to continue? [y/n]: ";
+            cin >> cont;
+        }
+
+    } while (choice != '7' && cont == 'y');
+
+    return 0;
+}

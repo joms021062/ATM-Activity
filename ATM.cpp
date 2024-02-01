446fd7222710898521a781ae0661471931ba5552
joms021062 <156154075+joms021062@users.noreply.github.com>
2024-02-01 16:39:34 +0800

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -4,48 +4,46 @@ using namespace std;
 
 int main() {
 
-	int stored = 1000;
 	int input;
-	char choice;
-	char cont;
+	char choice, cont;
 
 	do {
-		cout << "\nMenu\n";
+		cout << "=============================\n";
+		cout << "          WELCOME!            ";
+		cout << "\n=============================";
+		cout << "\n           Menu            \n";
 		cout << "1. Deposit" << endl;
 		cout << "2. Withdraw" << endl;
-		cout << "3. Exit" << endl;
-		cout << "\nEnter your choice: ";0
-		cin >> choice;
+		cout << "4. Exit" << endl;
+		cout << "=============================";
+		cout << "\nEnter your choice: ";
+			cin >> choice;
 
 		switch (choice) {
 		case '1':
-			cout << "Enter value to Deposit: ";
+			cout << "\nEnter value to Deposit: ";
 			cin >> input;
-			stored += input;
-			cout << "Your balance is: " << stored << endl;
 			break;
 
 		case '2':
-			cout << "Enter value to withdraw: ";
+			cout << "\nEnter value to withdraw: ";
 			cin >> input;
-			stored -= input;
-			cout << "Your balance is: " << stored << endl;
 			break;
 
-		case '3':
-			cout << "Exiting... " << endl;
+		case '4':
+			cout << "\nExiting... " << endl;
 			break;
 
 		default:
-			cout << "Invalid Choice. Please Enter Again." << endl;
+			cout << "\nInvalid Choice. Please Try Again." << endl;
 		}
-		
-		if (choice != '3') {
+
+		if (choice != '4') {
 			cout << "\nDo you want another transaction?[y/n]:";
 			cin >> cont;
 		}
 
-	} while (choice != '3' && cont == 'y');
+	} while (choice != '4' && cont == 'y');
 
 	return 0;
 
